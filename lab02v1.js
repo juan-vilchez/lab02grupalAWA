13fd8d6cfb276d354745f4aacdd8e85c68ea0d09
pieremcustodio <piere.custodio@tecsup.edu.pe>
2020-10-02 17:48:59 -0500

    Agregando sobre nosotros y realizando configuraciones

--- lab02v1.js
+++ lab02v1.js
@@ -1,11 +1,48 @@
-var http = require('http');
-fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 8080;
 
-var html = fs.readFileSync('./index.html');
+const server = http.createServer((req, res) => {
+    let filePath = path.join(
+        __dirname,
+        req.url === "/" ? "servicios.html" : req.url
+    );
 
+    let extName = path.extname(filePath);
+    let contentType = 'text/html';
 
-http.createServer(function(solicitud, respuesta){
-    respuesta.write(html);
-    respuesta.end();
+    switch (extName) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
 
-}).listen(8080);
+    console.log(`File path: ${filePath}`);
+    console.log(`Content-Type: ${contentType}`)
+
+    res.writeHead(200, {'Content-Type': contentType});
+
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(res);
+});
+
+server.listen(port, (err) => {
+    if (err) {
+        console.log(`Error: ${err}`)
+    } else {
+        console.log(`Server listening at port ${port}...`);
+    }
+});
